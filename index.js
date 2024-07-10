6e94496fe003dbc477a18286b7795b64952981af
Uttam <129719033+uttambodara@users.noreply.github.com>
2024-07-10 18:49:53 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-export { renderRepoCard } from "./repo-card.js";
-export { renderStatsCard } from "./stats-card.js";
-export { renderTopLanguages } from "./top-languages-card.js";
-export { renderWakatimeCard } from "./wakatime-card.js";
+export { blacklist } from "./blacklist.js";
+export { Card } from "./Card.js";
+export { createProgressNode } from "./createProgressNode.js";
+export { I18n } from "./I18n.js";
+export { icons } from "./icons.js";
+export { retryer } from "./retryer.js";
+export {
+  ERROR_CARD_LENGTH,
+  renderError,
+  encodeHTML,
+  kFormatter,
+  isValidHexColor,
+  parseBoolean,
+  parseArray,
+  clampValue,
+  isValidGradient,
+  fallbackColor,
+  request,
+  flexLayout,
+  getCardColors,
+  wrapTextMultiline,
+  logger,
+  CONSTANTS,
+  CustomError,
+  MissingParamError,
+  measureText,
+  lowercaseTrim,
+  chunkArray,
+  parseEmojis,
+} from "./utils.js";
--- types.d.ts
+++ types.d.ts
@@ -1,63 +1,118 @@
-type ThemeNames = keyof typeof import("../../themes/index.js");
-type RankIcon = "default" | "github" | "percentile";
-
-export type CommonOptions = {
-  title_color: string;
-  icon_color: string;
-  text_color: string;
-  bg_color: string;
-  theme: ThemeNames;
-  border_radius: number;
-  border_color: string;
-  locale: string;
-  hide_border: boolean;
+export type GistData = {
+  name: string;
+  nameWithOwner: string;
+  description: string | null;
+  language: string | null;
+  starsCount: number;
+  forksCount: number;
 };
 
-export type StatCardOptions = CommonOptions & {
-  hide: string[];
-  show_icons: boolean;
-  hide_title: boolean;
-  card_width: number;
-  hide_rank: boolean;
-  include_all_commits: boolean;
-  line_height: number | string;
-  custom_title: string;
-  disable_animations: boolean;
-  number_format: string;
-  ring_color: string;
-  text_bold: boolean;
-  rank_icon: RankIcon;
-  show: string[];
+export type RepositoryData = {
+  name: string;
+  nameWithOwner: string;
+  isPrivate: boolean;
+  isArchived: boolean;
+  isTemplate: boolean;
+  stargazers: { totalCount: number };
+  description: string;
+  primaryLanguage: {
+    color: string;
+    id: string;
+    name: string;
+  };
+  forkCount: number;
+  starCount: number;
 };
 
-export type RepoCardOptions = CommonOptions & {
-  show_owner: boolean;
-  description_lines_count: number;
+export type StatsData = {
+  name: string;
+  totalPRs: number;
+  totalPRsMerged: number;
+  mergedPRsPercentage: number;
+  totalReviews: number;
+  totalCommits: number;
+  totalIssues: number;
+  totalStars: number;
+  totalDiscussionsStarted: number;
+  totalDiscussionsAnswered: number;
+  contributedTo: number;
+  rank: { level: string; percentile: number };
 };
 
-export type TopLangOptions = CommonOptions & {
-  hide_title: boolean;
-  card_width: number;
-  hide: string[];
-  layout: "compact" | "normal" | "donut" | "donut-vertical" | "pie";
-  custom_title: string;
-  langs_count: number;
-  disable_animations: boolean;
-  hide_progress: boolean;
+export type Lang = {
+  name: string;
+  color: string;
+  size: number;
 };
 
-export type WakaTimeOptions = CommonOptions & {
-  hide_title: boolean;
-  hide: string[];
-  line_height: string;
-  hide_progress: boolean;
-  custom_title: string;
-  layout: "compact" | "normal";
-  langs_count: number;
-  display_format: "time" | "percent";
-  disable_animations: boolean;
+export type TopLangData = Record<string, Lang>;
+
+export type WakaTimeData = {
+  categories: {
+    digital: string;
+    hours: number;
+    minutes: number;
+    name: string;
+    percent: number;
+    text: string;
+    total_seconds: number;
+  }[];
+  daily_average: number;
+  daily_average_including_other_language: number;
+  days_including_holidays: number;
+  days_minus_holidays: number;
+  editors: {
+    digital: string;
+    hours: number;
+    minutes: number;
+    name: string;
+    percent: number;
+    text: string;
+    total_seconds: number;
+  }[];
+  holidays: number;
+  human_readable_daily_average: string;
+  human_readable_daily_average_including_other_language: string;
+  human_readable_total: string;
+  human_readable_total_including_other_language: string;
+  id: string;
+  is_already_updating: boolean;
+  is_coding_activity_visible: boolean;
+  is_including_today: boolean;
+  is_other_usage_visible: boolean;
+  is_stuck: boolean;
+  is_up_to_date: boolean;
+  languages: {
+    digital: string;
+    hours: number;
+    minutes: number;
+    name: string;
+    percent: number;
+    text: string;
+    total_seconds: number;
+  }[];
+  operating_systems: {
+    digital: string;
+    hours: number;
+    minutes: number;
+    name: string;
+    percent: number;
+    text: string;
+    total_seconds: number;
+  }[];
+  percent_calculated: number;
+  range: string;
+  status: string;
+  timeout: number;
+  total_seconds: number;
+  total_seconds_including_other_language: number;
+  user_id: string;
+  username: string;
+  writes_only: boolean;
 };
 
-export type GistCardOptions = CommonOptions & {
-  show_owner: boolean;
+export type WakaTimeLang = {
+  name: string;
+  text: string;
+  percent: number;
 };
